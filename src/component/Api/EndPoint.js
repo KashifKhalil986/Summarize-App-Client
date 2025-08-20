12874e55d9280d4ac502406fe1cf0aeb093c6817
KashifKhalil986 <kashifkhalil720@gmail.com>
2025-08-20 12:04:32 +0500

    added delete functionality to remove a summary

--- src/component/Api/EndPoint.js
+++ src/component/Api/EndPoint.js
@@ -3,3 +3,4 @@ export const Sign_Up_End_Point ="/register";
 export const Change_Password_End_Point ="/change-password";
 export const Summary_Get_End_Point ="/get-summary";
 export const Summary_Post_End_Point ="/post-summary";
+export const Summary_Delete_End_Point ="/:id";
